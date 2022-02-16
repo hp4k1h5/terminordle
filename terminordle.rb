031f28d1355b1619d598b4182eb8a82daabfac72
bob <robertwalks@gmail.com>
2022-02-16 10:56:13 -0500

    edit: update brew

--- terminordle.rb
+++ terminordle.rb
@@ -5,8 +5,8 @@ require "language/node"
 class Terminordle < Formula
   desc "Multiplayer wordle in the terminal"
   homepage "https://github.com/HP4k1h5/terminordle/"
-  url "https://registry.npmjs.org/@hp4k1h5/terminordle/-/terminordle-0.1.0.tgz"
-  sha256 "f1153c34b8da36c3269e8d134707d27b7a72091ea0c90a658b759f889d64a1aa"
+  url "https://registry.npmjs.org/@hp4k1h5/terminordle/-/terminordle-0.1.1.tgz"
+  sha256 "131b6491dbdb14d0b8425c29c8c56f6c77870b9851be21d0e259befdf0507df6"
   license "MIT"
 
   depends_on "node"
